bdd5745733f8e76d059277a11a120190aa44bfe7
SubashAravindan <p.subasharavindan@gmail.com>
2018-12-03 23:35:53 +0530

    added eslint, route files

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,5 +1,17 @@
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const codeRoutes = require('./code');
+const userAuthRoutes = require('./userAuth');
+const userRoutes = require('./user');
+const leaderboardRoutes = require('./leaderboard');
+const simulationRoutes = require('./simulation');
+
+const router = express.Router();
+
+router.use('/code', codeRoutes);
+router.use('/user/profile', userRoutes);
+router.use('/user', userAuthRoutes);
+router.use('/leaderboard', leaderboardRoutes);
+router.use('/compete', simulationRoutes);
 
 
-module.exports = router
+module.exports = router;
