42f18d1fff817a6b05a2b28ad590f7fe1e57c225
SubashAravindan <p.subasharavindan@gmail.com>
2019-01-25 19:54:34 +0530

    Fix profile routes to return consistent responses

--- api/routes/user.js
+++ api/routes/user.js
@@ -5,28 +5,45 @@ const router = express.Router();
 
 router.get('/', (req, res) => {
   const { username, fullName, country } = req.user;
-  res.send({ username, fullName, country });
+  res.status(200).json({
+    type: 'Success',
+    error: '',
+    userDetails: { username, fullName, country },
+  });
 });
 
 router.get('/view/:username', async (req, res) => {
   const { username } = req.params;
   try {
-    const user = await User.findOne({
+    const userDetails = await User.findOne({
       where: { username },
       attributes: ['username', 'fullName', 'country'],
     });
-    res.send(user);
+    res.status(200).json({
+      type: 'Success',
+      error: '',
+      userDetails,
+    });
   } catch (error) {
-    res.status(500).send(error);
+    res.status(500).json({
+      type: 'Error',
+      error: 'Internal server error',
+    });
   }
 });
 
 router.post('/update', async (req, res) => {
   try {
     await User.update(req.body, { where: { id: req.user.id } });
-    res.sendStatus(200);
+    res.status(200).json({
+      type: 'Success',
+      error: '',
+    });
   } catch (error) {
-    res.status(500).json(error);
+    res.status(500).json({
+      type: 'Error',
+      error: 'Internal server error',
+    });
   }
 });
 
