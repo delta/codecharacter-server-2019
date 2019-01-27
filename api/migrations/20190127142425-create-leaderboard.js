ad6e30163ac8638780e6964e04c1f40b504565b6
mario-vimal <mariovimal@gmail.com>
2019-01-28 00:34:27 +0530

    Add leaderboard model and migration

---
+++ api/migrations/20190127142425-create-leaderboard.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('leaderboards', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      user_id: {
+        type: Sequelize.INTEGER
+      },
+      rating: {
+        type: Sequelize.INTEGER
+      },
+      dll1: {
+        type: Sequelize.STRING
+      },
+      dll2: {
+        type: Sequelize.STRING
+      },
+      is_ai: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('leaderboards');
+  }
+};
