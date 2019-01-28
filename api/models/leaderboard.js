789ca463b1ca9e071e8437256b97dcfd5d26f932
Mario Jones Vimal <mariovimal@gmail.com>
2019-01-28 22:21:06 +0530

    Add leaderboard model and migration

---
+++ api/models/leaderboard.js
@@ -0,0 +1,38 @@
+
+const User = require('./user');
+
+module.exports = (sequelize, DataTypes) => {
+  const leaderboard = sequelize.define('leaderboard', {
+    user_id: {
+      allowNull: false,
+      type: DataTypes.INTEGER,
+      unique: true,
+      foreignKey: true,
+      references: {
+        model: User,
+        key: 'id',
+      },
+    },
+    rating: {
+      type: DataTypes.INTEGER,
+      defaultValue: 1000,
+    },
+    dll1: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    dll2: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    is_ai: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: false,
+    },
+  }, {});
+  // leaderboard.associate = function(models) {
+  //   // associations can be defined here
+  // };
+  return leaderboard;
+};
