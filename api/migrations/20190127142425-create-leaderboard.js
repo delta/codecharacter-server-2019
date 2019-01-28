534f59688c495ed316da61f8d99c76cff2b6f16b
mario-vimal <mariovimal@gmail.com>
2019-01-28 19:18:18 +0530

    Add default values for leaderboard table

--- api/migrations/20190127142425-create-leaderboard.js
+++ api/migrations/20190127142425-create-leaderboard.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('leaderboards', {
+    return queryInterface.createTable('leaderboard', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -9,19 +9,24 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       user_id: {
+        allowNull: false,
         type: Sequelize.INTEGER
       },
       rating: {
-        type: Sequelize.INTEGER
+        type: Sequelize.INTEGER,
+        defaultValue: 1000
       },
       dll1: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false
       },
       dll2: {
-        type: Sequelize.STRING
+        type: Sequelize.STRING,
+        allowNull: false
       },
       is_ai: {
-        type: Sequelize.BOOLEAN
+        type: Sequelize.BOOLEAN,
+        defaultValue: false
       },
       createdAt: {
         allowNull: false,
@@ -34,6 +39,6 @@ module.exports = {
     });
   },
   down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('leaderboards');
+    return queryInterface.dropTable('leaderboard');
   }
 };
--- api/models/leaderboard.js
+++ api/models/leaderboard.js
@@ -7,7 +7,7 @@ module.exports = (sequelize, DataTypes) => {
       allowNull: false,
       type: DataTypes.INTEGER,
       unique: true,
-      foreignKEY: true,
+      foreignKey: true,
       references: {
         model: User,
         key: 'id',
