43d3e16ca28bddc6b4f004d5bdd658b9809f2567
mario-vimal <mariovimal@gmail.com>
2019-01-31 00:54:44 +0530

    Add Search

--- api/routes/leaderboard.js
+++ api/routes/leaderboard.js
@@ -1,17 +1,14 @@
 const express = require('express');
-// const { Op } = require('sequelize');
+const { Op } = require('sequelize');
 const Leaderboard = require('../models').leaderboard;
 const User = require('../models').user;
 
 Leaderboard.belongsTo(User, { foreignKey: 'user_id' });
 const router = express.Router();
-// let filtered;
-let index;
-// eslint-disable-next-line prefer-const
-let obj = {};
+let obj;
 router.post('/:start/:finish', async (req, res) => {
-  let { finish } = req.params;
-  const { start } = req.params;
+  obj = {};
+  const { start, finish } = req.params;
   const leaderboard = await Leaderboard.findAll(
     {
       include: [
@@ -25,22 +22,54 @@ router.post('/:start/:finish', async (req, res) => {
     },
   );
   if (leaderboard.length) {
-    for (index = 0; index < leaderboard.length; index += 1) {
-      if (index >= start && index < finish) {
-        obj[index - start] = {};
-        obj[index - start].rank = index + 1;
-        obj[index - start].rating = leaderboard[index].rating;
-        obj[index - start].fullName = leaderboard[index].user.fullName;
-        obj[index - start].country = leaderboard[index].user.country;
-      }
-    }
-    if (leaderboard.length < finish + 1) {
-      finish = leaderboard.length;
-    } else if (start > leaderboard.length) {
+    if (start > leaderboard.length) {
       return res.status(200).send({ type: 'Success', error: '', message: JSON.parse('') });
     }
-    // console.log(leaderboard.text());
-    // filtered = obj.slice(start, finish + 1);
+    for (let index = start; index < Math.min(finish, leaderboard.length); index += 1) {
+      obj[index - start] = {};
+      obj[index - start].rank = index + 1;
+      obj[index - start].rating = leaderboard[index].rating;
+      obj[index - start].fullName = leaderboard[index].user.fullName;
+      obj[index - start].country = leaderboard[index].user.country;
+    }
+    return res.status(200).send({ type: 'Success', error: '', message: JSON.stringify(obj) });
+  }
+  return res.status(400).json({
+    message: 'Error',
+    error: 'No Entries Found',
+  });
+});
+router.post('/:search/:start/:finish', async (req, res) => {
+  obj = {};
+  const { search, start, finish } = req.params;
+  const leaderboard = await Leaderboard.findAll(
+    {
+      include: [
+        {
+          model: User,
+          attributes: ['fullName', 'country'],
+          where: {
+            fullName: {
+              [Op.like]: `%${search}%`,
+            },
+          },
+        },
+      ],
+      order: ['rating'],
+      attributes: ['rating'],
+    },
+  );
+  if (leaderboard.length) {
+    if (start > leaderboard.length) {
+      return res.status(200).send({ type: 'Success', error: '', message: JSON.parse('') });
+    }
+    for (let index = start; index < Math.min(finish, leaderboard.length); index += 1) {
+      obj[index - start] = {};
+      obj[index - start].rank = index + 1;
+      obj[index - start].rating = leaderboard[index].rating;
+      obj[index - start].fullName = leaderboard[index].user.fullName;
+      obj[index - start].country = leaderboard[index].user.country;
+    }
     return res.status(200).send({ type: 'Success', error: '', message: JSON.stringify(obj) });
   }
   return res.status(400).json({
