aa0344f5527b949c9552c9c2c9fa2af95ac71489
Mario Jones Vimal <mariovimal@gmail.com>
2019-02-02 20:18:06 +0530

    Add Leaderboard routes
    - Add migration and model for Leaderboard

--- api/routes/leaderboard.js
+++ api/routes/leaderboard.js
@@ -1,5 +1,143 @@
 const express = require('express');
+const { check, validationResult } = require('express-validator/check');
 
+const Leaderboard = require('../models').leaderboard;
+const User = require('../models').user;
+
+Leaderboard.belongsTo(User, { foreignKey: 'user_id' });
 const router = express.Router();
+router.post('/:start/:finish', [
+  check('start')
+    .not().isEmpty().withMessage('Start cannot be empty')
+    .isInt()
+    .withMessage('Start must be Integer'),
+  check('finish')
+    .not().isEmpty().withMessage('Finish cannot be empty')
+    .isInt()
+    .withMessage('Finish must be Integer'),
+], async (req, res) => {
+  const leaderboardData = [];
+  let { start, finish } = req.params;
+  const errors = validationResult(req);
+
+  start = parseInt(start, 10);
+  finish = parseInt(finish, 10);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      type: 'Error',
+      error: (errors.array())[0],
+    });
+  }
+
+  if (start > finish) {
+    return res.status(400).json({
+      type: 'Error',
+      error: 'Start cannot be greater than Finish',
+    });
+  }
+
+  try {
+    const leaderboard = await Leaderboard.findAll(
+      {
+        include: [
+          {
+            model: User,
+            attributes: ['username', 'fullName', 'country'],
+          },
+        ],
+        order: ['rating'],
+        attributes: ['rating'],
+      },
+    );
+
+    finish = Math.min(finish, leaderboard.length);
+    for (let index = start - 1; index < finish; index += 1) {
+      const leaderboardElement = {};
+      leaderboardElement.rank = index + 1;
+      leaderboardElement.rating = leaderboard[index].rating;
+      leaderboardElement.fullName = leaderboard[index].user.fullName;
+      leaderboardElement.country = leaderboard[index].user.country;
+      leaderboardElement.username = leaderboard[index].user.username;
+      leaderboardData.push(leaderboardElement);
+    }
+    return res.status(200).json({ type: 'Success', error: '', leaderboardData });
+  } catch (err) {
+    return res.status(500).json({
+      type: 'Error',
+      error: 'Internal Server Error',
+    });
+  }
+});
+
+router.post('/:search/:start/:finish', [
+  check('search')
+    .not().isEmpty().withMessage('Search cannot be empty'),
+  check('start')
+    .not().isEmpty().withMessage('Start cannot be empty')
+    .isInt()
+    .withMessage('Start must be Integer'),
+  check('finish')
+    .not().isEmpty().withMessage('Finish cannot be empty')
+    .isInt()
+    .withMessage('Finish must be Integer'),
+], async (req, res) => {
+  const leaderboardData = [];
+  let { start, finish } = req.params;
+  const { search } = req.params;
+  const errors = validationResult(req);
+  let count = 0;
+
+  start = parseInt(start, 10);
+  finish = parseInt(finish, 10);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      type: 'Error',
+      error: (errors.array())[0],
+    });
+  }
+
+  if (start > finish) {
+    return res.status(400).json({
+      type: 'Error',
+      error: 'Start cannot be greater than Finish',
+    });
+  }
+
+  try {
+    const leaderboard = await Leaderboard.findAll(
+      {
+        include: [
+          {
+            model: User,
+            attributes: ['username', 'fullName', 'country'],
+          },
+        ],
+        order: ['rating'],
+        attributes: ['rating'],
+      },
+    );
+
+    finish = Math.min(finish, leaderboard.length);
+    for (let index = start - 1; index < finish; index += 1) {
+      if (leaderboard[index].user.fullName.includes(search)) {
+        const leaderboardElement = {};
+        leaderboardElement.rank = index + 1;
+        leaderboardElement.rating = leaderboard[index].rating;
+        leaderboardElement.fullName = leaderboard[index].user.fullName;
+        leaderboardElement.country = leaderboard[index].user.country;
+        leaderboardElement.username = leaderboard[index].user.username;
+        leaderboardData[count] = leaderboardElement;
+        count += 1;
+      }
+    }
+    return res.status(200).json({ type: 'Success', error: '', leaderboardData });
+  } catch (err) {
+    return res.status(500).json({
+      type: 'Error',
+      error: 'Internal Server Error',
+    });
+  }
+});
 
 module.exports = router;
---
+++ test/leaderboard.js
@@ -0,0 +1,173 @@
+process.env.NODE_ENV = 'test';
+
+const chai = require('chai');
+// eslint-disable-next-line
+const should = chai.should();
+const chaiHttp = require('chai-http');
+const request = require('supertest');
+
+const server = require('../app');
+const User = require('../api/models').user;
+const Leaderboard = require('../api/models').leaderboard;
+
+chai.use(chaiHttp);
+
+describe('Test Leaderboard', async () => {
+  describe('Test Leaderboard by Rank', async () => {
+    const superAgent = request.agent(server);
+    const searchKey = 't';
+    // eslint-disable-next-line no-undef
+    after(async () => {
+      const user = await User.findAll();
+      for (let index = 0; index < user.length; index += 1) {
+        user[index].destroy();
+      }
+      const leaderboard = await Leaderboard.findAll();
+      for (let index = 0; index < leaderboard.length; index += 1) {
+        leaderboard[index].destroy();
+      }
+    });
+    // eslint-disable-next-line no-undef
+    before(async () => {
+      const numEntries = 10;
+      const registerBody = {
+        username: 'testusername',
+        password: 'testpassword',
+        repeatPassword: 'testpassword',
+        email: 'testemail@test.com',
+        country: 'IN',
+        fullName: 'Mocha Test',
+        pragyanId: null,
+      };
+      await superAgent.post('/user/register')
+        .set('content-type', 'application/json')
+        .send(registerBody);
+      const loginBody = {
+        username: 'testusername',
+        password: 'testpassword',
+      };
+      await superAgent.post('/user/login')
+        .set('content-type', 'application/json')
+        .send(loginBody);
+      for (let index = 0; index < numEntries; index += 1) {
+        User.findOrCreate({
+          where: {
+            username: `testusername${index}`,
+            password: `testpassword${index}`,
+            email: `testemail${index}@testemail.com`,
+            fullName: `Mocha${index}`,
+            id: 100 + index,
+          },
+        });
+      }
+      for (let index = 0; index < numEntries; index += 1) {
+        User.findOrCreate({
+          where: {
+            username: `test${index}`,
+            password: `test${index}`,
+            email: `test${index}@testemail.com`,
+            fullName: `test${index}`,
+            id: 110 + index,
+          },
+        });
+      }
+      for (let index = 0; index < numEntries; index += 1) {
+        Leaderboard.findOrCreate({
+          where: {
+            user_id: 100 + index,
+            rating: 100 * index,
+            dll1: `dll1${index}`,
+            dll2: `dll2${index}`,
+          },
+        });
+      }
+      for (let index = 0; index < numEntries; index += 1) {
+        Leaderboard.findOrCreate({
+          where: {
+            user_id: 110 + index,
+            rating: 100 * index + 50,
+            dll1: `dll1${index}`,
+            dll2: `dll2${index}`,
+          },
+        });
+      }
+    });
+
+    it('start must be int', async () => {
+      const start = 'a';
+      const finish = 1;
+      const { res } = await superAgent
+        .post(`/leaderboard/${start}/${finish}`);
+
+      res.should.have.status(400);
+      chai.assert(JSON.parse(res.text).error.msg === 'Start must be Integer');
+    });
+
+    it('finish must be int', async () => {
+      const start = 0;
+      const finish = 'a';
+      const { res } = await superAgent
+        .post(`/leaderboard/${start}/${finish}`);
+      res.should.have.status(400);
+      chai.assert(JSON.parse(res.text).error.msg === 'Finish must be Integer');
+    });
+
+
+    it('send 400 (Start greater than Finish)', async () => {
+      const start = 2;
+      const finish = 1;
+      const { res } = await superAgent
+        .post(`/leaderboard/${start}/${finish}`);
+      res.should.have.status(400);
+      chai.assert(JSON.parse(res.text).error === 'Start cannot be greater than Finish');
+    });
+
+    it('send 200 (Rank 1)', async () => {
+      const start = 1;
+      const finish = 1;
+      const { res } = await superAgent
+        .post(`/leaderboard/${start}/${finish}`);
+      res.should.have.status(200);
+    });
+
+    it('send 200 (Ranks 5-10)', async () => {
+      const start = 5;
+      const finish = 10;
+      const { res } = await superAgent
+        .post(`/leaderboard/${start}/${finish}`);
+      res.should.have.status(200);
+      const { leaderboardData } = JSON.parse(res.text);
+      for (let index = 0; index < leaderboardData.length; index += 1) {
+        chai.assert(leaderboardData[index].rank === start + index);
+      }
+    });
+
+    it('send 200 (Ranks 1-100)', async () => {
+      const start = 1;
+      const finish = 100;
+      const numEntries = 20;
+      const { res } = await superAgent
+        .post(`/leaderboard/${start}/${finish}`);
+      res.should.have.status(200);
+      const { leaderboardData } = JSON.parse(res.text);
+      chai.assert(leaderboardData.length === numEntries);
+      for (let index = 0; index < numEntries; index += 1) {
+        chai.assert(leaderboardData[index].rank === start + index);
+      }
+    });
+
+    it(`send 200 (Search '${searchKey}')`, async () => {
+      const start = 1;
+      const finish = 100;
+      const numEntries = 10;
+      const { res } = await superAgent
+        .post(`/leaderboard/${searchKey}/${start}/${finish}`);
+      res.should.have.status(200);
+      const { leaderboardData } = JSON.parse(res.text);
+      chai.assert(leaderboardData.length === numEntries);
+      for (let index = 0; index < numEntries; index += 1) {
+        chai.assert(leaderboardData[index].rank === 2 + index * 2);
+      }
+    });
+  });
+});
