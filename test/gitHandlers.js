9d2247eef1663134f857a6856ffc3a4757f1d060
mario-vimal <mariovimal@gmail.com>
2019-02-09 00:57:24 +0530

    Fix await fs write

--- test/gitHandlers.js
+++ test/gitHandlers.js
@@ -6,7 +6,10 @@ const shell = require('shelljs');
 const git = require('simple-git/promise');
 const fs = require('fs');
 const randomString = require('randomstring');
+const util = require('util');
 const GitHandler = require('../api/utils/gitHandlers');
+
+const fsWriteFile = util.promisify(fs.writeFile);
 
 appPath = path.resolve('../codecharacter-server-2019/');
 const getUserDir = username => `${appPath}/storage/codes/${username}`;
@@ -14,8 +17,8 @@ const getUserDir = username => `${appPath}/storage/codes/${username}`;
 describe('Test Git Handlers', async () => {
   const username = 'gitHandler_testusername';
   const userDir = getUserDir(username);
-  const InitialCommitMessage = 'Initial Commit';
-  const text = new Array(10);
+  const initialCommitMessage = 'Initial Commit';
+  const text = new Array(8);
   // eslint-disable-next-line no-undef
   before(async () => {
     for (let index = 0; index < 10; index += 1) {
@@ -30,12 +33,12 @@ describe('Test Git Handlers', async () => {
     const handlerResponse = await GitHandler.createUserDir(username);
     chai.assert(await shell.test('-d', userDir) === true);
     const logResult = await git(userDir).log();
-    chai.assert(await logResult.latest.message.includes(InitialCommitMessage));
+    chai.assert(await logResult.latest.message.includes(initialCommitMessage));
     chai.assert(handlerResponse === true);
   });
 
   it('test latestCommit', async () => {
-    fs.writeFile(`${userDir}/code.cpp`, text[0], async () => {});
+    await fsWriteFile(`${userDir}/code.cpp`, text[0]);
     await git(userDir).init();
     await git(userDir).add('./*');
     await git(userDir).commit(text[0]);
@@ -44,14 +47,14 @@ describe('Test Git Handlers', async () => {
   });
 
   it('test commitLog', async () => {
-    fs.writeFile(`${userDir}/code.cpp`, text[1], async () => {});
+    await fsWriteFile(`${userDir}/code.cpp`, text[1]);
     await git(userDir).init();
     await git(userDir).add('./*');
     await git(userDir).commit(text[1]);
     const commitLog = await GitHandler.commitLog(username);
     chai.assert(commitLog[0].message.includes(text[1]));
     chai.assert(commitLog[1].message.includes(text[0]));
-    chai.assert(commitLog[2].message.includes(InitialCommitMessage));
+    chai.assert(commitLog[2].message.includes(initialCommitMessage));
   });
 
   it('test add', async () => {
@@ -59,7 +62,7 @@ describe('Test Git Handlers', async () => {
     let logResult = await git(userDir).log();
     let latestCommit = logResult.latest;
     chai.assert(!latestCommit.message.includes(text[2]));
-    fs.writeFile(`${userDir}/code.cpp`, text[2], async () => {});
+    await fsWriteFile(`${userDir}/code.cpp`, text[2]);
     await GitHandler.add(username);
     await git(userDir).commit(text[2]);
     logResult = await git(userDir).log();
@@ -70,13 +73,13 @@ describe('Test Git Handlers', async () => {
   it('test diff', async () => {
     let diff = await GitHandler.diff(username);
     chai.assert(diff === '');
-    fs.writeFile(`${userDir}/code.cpp`, text[3], async () => {});
+    await fsWriteFile(`${userDir}/code.cpp`, text[3]);
     diff = await GitHandler.diff(username);
     chai.assert(diff !== '');
   });
 
   it('test diff staged', async () => {
-    fs.writeFile(`${userDir}/code.cpp`, text[4], async () => {});
+    await fsWriteFile(`${userDir}/code.cpp`, text[4]);
     let diff = await GitHandler.diffStaged(username);
     chai.assert(diff === '');
     await git(userDir).init();
@@ -86,7 +89,7 @@ describe('Test Git Handlers', async () => {
   });
 
   it('test commit', async () => {
-    fs.writeFile(`${userDir}/code.cpp`, text[5], async () => {});
+    await fsWriteFile(`${userDir}/code.cpp`, text[5]);
     await git(userDir).init();
     await git(userDir).add('./*');
     await GitHandler.commit(username, text[5]);
@@ -96,7 +99,7 @@ describe('Test Git Handlers', async () => {
   });
 
   it('test getFile', async () => {
-    fs.writeFile(`${userDir}/code.cpp`, text[6], async () => {});
+    await fsWriteFile(`${userDir}/code.cpp`, text[6]);
     const log = await git(userDir).log();
     let file = await GitHandler.getFile(username, 'code.cpp', log.latest.hash);
     chai.assert(file === text[5]);
