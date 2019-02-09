5d0f7e4c5ac75ca66e4a3c97bfdf78b089fb0432
mario-vimal <mariovimal@gmail.com>
2019-02-09 21:23:47 +0530

    Fix await inside loop

--- test/notification.js
+++ test/notification.js
@@ -1,116 +1,158 @@
 process.env.NODE_ENV = 'test';
 
 const chai = require('chai');
-// eslint-disable-next-line
-const should = chai.should();
-const chaiHttp = require('chai-http');
 const request = require('supertest');
 
 const server = require('../app');
 const User = require('../api/models').user;
 const Notification = require('../api/models').notification;
-
-chai.use(chaiHttp);
+const CodeStatus = require('../api/models').codestatus;
 
 describe('Test Notification', async () => {
   const superAgent = request.agent(server);
   const numEntries = 20;
+  const getType = (index) => {
+    if (index < 5) {
+      return 'Info';
+    }
+    if (index < 10) {
+      return 'Success';
+    }
+    return 'Error';
+  };
   // eslint-disable-next-line no-undef
   before(async () => {
-    for (let index = 0; index < numEntries; index += 1) {
-      // eslint-disable-next-line no-unused-vars
-      const registerBody = {
-        username: `notification_test_${index}`,
-        password: 'password',
-        repeatPassword: 'password',
-        email: `notification${index}@test.com`,
-        fullName: 'Notification',
-        country: 'IN',
-      };
-      // eslint-disable-next-line no-await-in-loop
-      await superAgent.post('/user/register')
-        .set('content-type', 'application/json')
-        .send(registerBody);
-    }
+    const registerBody = {
+      username: 'notification_test',
+      password: 'password',
+      repeatPassword: 'password',
+      email: 'notification@test.com',
+      fullName: 'Notification',
+      country: 'IN',
+    };
+    await superAgent.post('/user/register')
+      .set('content-type', 'application/json')
+      .send(registerBody);
   });
   beforeEach(async () => {
+    const user = await User.findOne({
+      where: {
+        username: 'notification_test',
+      },
+    });
+    const notificationResults = [];
     for (let index = 0; index < numEntries; index += 1) {
-      // eslint-disable-next-line no-await-in-loop
-      const user = await User.findOne({
+      const notification = Notification.findOrCreate({
         where: {
-          username: `notification_test_${index}`,
-        },
-      });
-
-      // eslint-disable-next-line no-await-in-loop
-      await Notification.findOrCreate({
-        where: {
-          id: user.id,
+          id: index + 1,
           title: `notification_title_${index}`,
           content: `notification_content_${index}`,
-          // eslint-disable-next-line no-nested-ternary
-          type: (index < 5) ? 'Info' : (index < 10) ? 'Success' : 'Error',
-          userId: user.id,
+          type: getType(index),
+          userId: JSON.parse(JSON.stringify(user)).id,
         },
       });
+      notificationResults.push(notification);
     }
+    await Promise.all(notificationResults);
   });
   // eslint-disable-next-line no-undef
   after(async () => {
+    const user = await User.findOne({
+      where: {
+        username: 'notification_test',
+      },
+    });
+    await CodeStatus.destroy({
+      where: {
+        userId: user.id,
+      },
+    });
+    const notificationResults = [];
     for (let index = 0; index < numEntries; index += 1) {
-      // eslint-disable-next-line no-await-in-loop
-      const user = await User.findOne({
+      const notification = Notification.destroy({
         where: {
-          username: `notification_test_${index}`,
+          id: index + 1,
         },
       });
-      // eslint-disable-next-line no-await-in-loop
-      await user.destroy();
+      notificationResults.push(notification);
     }
+    await Promise.all(notificationResults);
+    await user.destroy();
   });
+
   it('Test delete by id', async () => {
+    const loginBody = {
+      username: 'notification_test',
+      password: 'password',
+    };
+    await superAgent.post('/user/login')
+      .set('content-type', 'application/json')
+      .send(loginBody);
+    const user = await User.findOne({
+      where: {
+        username: 'notification_test',
+      },
+    });
+    let notification = await Notification.findAll({
+      where: {
+        userId: JSON.parse(JSON.stringify(user)).id,
+      },
+    });
+    chai.assert(JSON.parse(JSON.stringify(notification)).length === 20);
+    const notificationResults = [];
     for (let index = 0; index < numEntries; index += 1) {
-      const loginBody = {
-        username: `notification_test_${index}`,
-        password: 'password',
-      };
+      const del = superAgent
+        .delete(`/notifications/delete/${index + 1}`);
 
-      // eslint-disable-next-line no-await-in-loop
-      await superAgent.post('/user/login')
-        .set('content-type', 'application/json')
-        .send(loginBody);
-
-      // eslint-disable-next-line no-await-in-loop
-      const user = await User.findOne({
-        where: {
-          username: `notification_test_${index}`,
-        },
-      });
-      // eslint-disable-next-line no-await-in-loop
-      const { res } = await superAgent
-        .delete(`/notifications/delete/${user.id}`);
-
-      res.should.have.status(200);
+      notificationResults.push(del);
     }
+    await Promise.all(notificationResults);
+    notification = await Notification.findAll({
+      where: {
+        userId: JSON.parse(JSON.stringify(user)).id,
+      },
+    });
+    chai.assert(JSON.parse(JSON.stringify(notification)).length === 0);
   });
 
   it('Test Delete by Type', async () => {
+    const loginBody = {
+      username: 'notification_test',
+      password: 'password',
+    };
+    await superAgent.post('/user/login')
+      .set('content-type', 'application/json')
+      .send(loginBody);
+    const user = await User.findOne({
+      where: {
+        username: 'notification_test',
+      },
+    });
     let type = 'Info';
-    for (let index = 0; index < numEntries; index += 1) {
-      // eslint-disable-next-line no-nested-ternary
-      type = (index < 5) ? 'Info' : (index < 10) ? 'Success' : 'Error';
-      const loginBody = {
-        username: `notification_test_${index}`,
-        password: 'password',
-      };
-      // eslint-disable-next-line no-await-in-loop
-      await superAgent.post('/user/login')
-        .set('content-type', 'application/json')
-        .send(loginBody);
+    await superAgent.delete(`/notifications/delete/type/${type}`);
+    let notification = await Notification.findAll({
+      where: {
+        userId: JSON.parse(JSON.stringify(user)).id,
+      },
+    });
+    chai.assert(JSON.parse(JSON.stringify(notification)).length === 15);
 
-      // eslint-disable-next-line no-await-in-loop
-      const { res } = await superAgent.delete(`/notifications/delete/type/${type}`);
-      res.should.have.status(200);
-    }
+    type = 'Success';
+    await superAgent.delete(`/notifications/delete/type/${type}`);
+    notification = await Notification.findAll({
+      where: {
+        userId: JSON.parse(JSON.stringify(user)).id,
+      },
+    });
+    chai.assert(JSON.parse(JSON.stringify(notification)).length === 10);
+
+    type = 'Error';
+    await superAgent.delete(`/notifications/delete/type/${type}`);
+    notification = await Notification.findAll({
+      where: {
+        userId: JSON.parse(JSON.stringify(user)).id,
+      },
+    });
+    chai.assert(JSON.parse(JSON.stringify(notification)).length === 0);
   });
 });
