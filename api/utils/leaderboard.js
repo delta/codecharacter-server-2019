af31c456b79579992f8a2e647f77e8f5e9e8e232
Sai Hemanth B <saihemanth9019@gmail.com>
2019-02-10 19:55:33 +0530

    Add /code/lock route to lock user dlls

---
+++ api/utils/leaderboard.js
@@ -0,0 +1,54 @@
+const shell = require('shelljs');
+const Leaderboard = require('../models').leaderboard;
+const Constant = require('../models').constant;
+const git = require('./gitHandlers');
+
+const createLeaderboardUserFolder = async (userDir) => {
+  await shell.mkdir(userDir);
+  await shell.touch(`${userDir}/dll1.dll`);
+  await shell.touch(`${userDir}dll2.dll`);
+};
+
+const updateLeaderboard = async (username) => {
+  const dll1 = await git.getFile(username, 'dll1.dll');
+  const dll2 = await git.getFile(username, 'dll2.dll');
+
+  const leaderboardStorageDir = (await Constant.findOne({
+    where: {
+      key: 'DEFAULT_LEADERBOARD_STORAGE_DIR',
+    },
+  })).value;
+
+  await git.setFile(username, 'dll1.dll', dll1, `${leaderboardStorageDir}/${username}`);
+  await git.setFile(username, 'dll2.dll', dll2, `${leaderboardStorageDir}/${username}`);
+};
+
+const createLeaderboardEntry = async (userId, username) => {
+  const initialRating = Number((await Constant.findOne({
+    where: {
+      key: 'INITIAL_RATING',
+    },
+  })).value);
+
+  const leaderboardStorageDir = (await Constant.findOne({
+    where: {
+      key: 'DEFAULT_LEADERBOARD_STORAGE_DIR',
+    },
+  })).value;
+
+  const leaderboardUserDir = `${leaderboardStorageDir}/${username}`;
+  await Leaderboard.create({
+    userId,
+    rating: initialRating,
+    dll1: `${leaderboardUserDir}/dll1.dll`,
+    dll2: `${leaderboardUserDir}/dll2.dll`,
+    isBot: false,
+  });
+
+  await createLeaderboardUserFolder(leaderboardUserDir);
+};
+
+module.exports = {
+  createLeaderboardEntry,
+  updateLeaderboard,
+};
