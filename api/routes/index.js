4721bfb966e04ef63c53157d619726a0b4e43c1b
Sai Hemanth B <saihemanth9019@gmail.com>
2019-02-10 15:39:31 +0530

    Add route /simulate/compile to compile

--- api/routes/index.js
+++ api/routes/index.js
@@ -14,7 +14,6 @@ router.use('/user/profile', isLoggedIn, userRoutes);
 router.use('/user', userAuthRoutes);
 router.use('/leaderboard', isLoggedIn, leaderboardRoutes);
 router.use('/notifications', isLoggedIn, notificationRoutes);
-router.use('/compete', isLoggedIn, simulationRoutes);
-
+router.use('/simulate', isLoggedIn, simulationRoutes);
 
 module.exports = router;
--- api/routes/simulation.js
+++ api/routes/simulation.js
@@ -1,5 +1,33 @@
 const express = require('express');
+const compileUtils = require('../utils/compile');
+const codeStatusUtils = require('../utils/codeStatus');
+const jobUtils = require('../utils/job');
 
 const router = express.Router();
+
+router.post('/compile', async (req, res) => {
+  const { id } = req.user;
+  try {
+    if ((await codeStatusUtils.getUserCodeStatus(id)) !== 'Idle') {
+      return res.status(400).json({
+        type: 'Error',
+        error: 'Job already in queue',
+      });
+    }
+
+    await compileUtils.pushToCompileQueue(id);
+    jobUtils.sendJob();
+
+    return res.status(200).json({
+      type: 'Success',
+      error: '',
+    });
+  } catch (err) {
+    return res.status(500).json({
+      type: 'Error',
+      error: 'Internal Server Error',
+    });
+  }
+});
 
 module.exports = router;
