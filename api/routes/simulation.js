a07aa69f5dadf37eaaef3a9940ace9cbf99dd3fd
Sai Hemanth B <saihemanth9019@gmail.com>
2019-02-11 20:38:35 +0530

    Add self match route
    - Fix absent mapId bug in executequeue route
    - Rename getStatus in compileBox utils to getCompileBoxStatus
    - Fix updateMatchResults bug which always updated the first match
    - Add socket messages for match routes
    - Add validation for match routes

--- api/routes/simulation.js
+++ api/routes/simulation.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const { check } = require('express-validator/check');
+const { handleValidationErrors } = require('../utils/validation');
 const compileUtils = require('../utils/compile');
 const codeStatusUtils = require('../utils/codeStatus');
 const jobUtils = require('../utils/job');
 const matchUtils = require('../utils/match');
+const executeUtils = require('../utils/execute');
 
 const router = express.Router();
 
@@ -31,10 +34,15 @@ router.post('/compile', async (req, res) => {
   }
 });
 
-router.post('/match', async (req, res) => {
+router.post('/match', [
+  check('opponentId')
+    .not().isEmpty().withMessage('opponentId cannot be empty')
+    .isInt(),
+], async (req, res) => {
+  if (handleValidationErrors(req, res)) return null;
+
   const { id } = req.user;
-  let { opponentId } = req.body;
-  opponentId = Number(opponentId);
+  const { opponentId } = req.body;
 
   try {
     const startMatchResponse = await matchUtils.startMatch(id, opponentId);
@@ -49,6 +57,39 @@ router.post('/match', async (req, res) => {
     return res.status(400).json({
       type: 'Error',
       error: startMatchResponse.message,
+    });
+  } catch (err) {
+    return res.status(500).json({
+      type: 'Error',
+      error: 'Internal Server Error',
+    });
+  }
+});
+
+router.post('/match/self', [
+  check('mapId')
+    .not().isEmpty().withMessage('mapId cannot be empty')
+    .isInt(),
+], async (req, res) => {
+  if (handleValidationErrors(req, res)) return null;
+
+  const { id } = req.user;
+  const { mapId } = req.body;
+
+  try {
+    const result = await executeUtils.pushSelfMatchToQueue(id, mapId);
+
+    if (result) {
+      jobUtils.sendJob();
+      return res.status(200).json({
+        type: 'Success',
+        error: '',
+      });
+    }
+
+    return res.status(400).json({
+      type: 'Error',
+      error: 'No compiled DLLs',
     });
   } catch (err) {
     return res.status(500).json({
