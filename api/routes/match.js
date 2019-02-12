408f109f115d6d696cd121a6d357cf5f70c99839
mario-vimal <mariovimal@gmail.com>
2019-02-13 05:10:26 +0530

    Add GET pro matches

--- api/routes/match.js
+++ api/routes/match.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const Match = require('../models').match;
+const Leaderboard = require('../models').leaderboard;
 const User = require('../models').user;
 
 const router = express.Router();
@@ -33,6 +34,44 @@ router.get('/all', async (req, res) => {
     matchData.push(matchEntry);
   }
   return res.status(200).json({ type: 'Success', error: '', matchData });
+});
+
+router.get('/pro', async (req, res) => {
+  let leaderboard = await Leaderboard.findAll({
+    include: {
+      model: User,
+    },
+    order: ['rating'],
+  });
+  leaderboard = parsify(leaderboard);
+  let allMatches = await Match.findAll({
+    where: {
+      status: 'DONE',
+    },
+  });
+  allMatches = parsify(allMatches);
+  let count = 10;
+  const result = [];
+  for (let diff = 0; diff <= 5; diff += 1) {
+    for (let index = 0; index < allMatches.length; index += 1) {
+      const match = allMatches[index];
+      if (Math.abs(match.score1 - match.score2) === diff) {
+        for (let player = 0; player < leaderboard.length; player += 1) {
+          const curPlayer = leaderboard[player];
+          if (match.userId1 === curPlayer.userId || match.userId2 === curPlayer.userId) {
+            if (!allMatches[index].isUsed) {
+              if (count !== 0) {
+                result.push(match);
+                allMatches[index].isUsed = true;
+                count -= 1;
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+  return res.status(200).json({ type: 'Success', error: '', matchData: result });
 });
 
 module.exports = router;
